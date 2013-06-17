ba1d100dc0254bbf71078ff6c9073be64c0224c2
Jared Beck <jared@jaredbeck.com>
2013-06-17 01:47:59 -0400

    handle case when calendar table is second, not first (on page)

--- lib/parser.rb
+++ lib/parser.rb
@@ -8,10 +8,29 @@ module KgsPlayersToMonths
     end
 
     def month_urls
-      murls = @doc.css('table.grid td a').map { |a| a['href'] }
-      puts sprintf "parsed: %d month urls", murls.length
-      murls
+      if calendar_table.nil?
+        puts "parser: no table of months"
+        []
+      else
+        murls = calendar_table.css('td a').map { |a| a['href'] }
+        puts sprintf "parsed: %d month urls", murls.length
+        valid_urls = murls.select { |url| valid_month_url?(url) }
+        puts sprintf "valid: %d urls", valid_urls.length
+        valid_urls
+      end
     end
 
+    private
+
+    def calendar_table
+      @doc.css('table.grid').last
+    end
+
+    def valid_month_url? url
+      /^gameArchives\.jsp
+        \?user=[a-zA-Z0-9]+
+        &year=[0-9]+
+        &month=[0-9]+$/x =~ url
+    end
   end
 end
